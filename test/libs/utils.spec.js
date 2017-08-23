409b49fd428242878e56254cbf6ad47d4f8fb0bb
Jamie Holdroyd <jamie@moltin.com>
2017-08-24 00:09:28 +0100

    Update util tests

--- test/libs/utils.spec.js
+++ test/libs/utils.spec.js
@@ -1,11 +1,48 @@
 'use strict';
 
+// Load chai
+const chai = require('chai');
+const expect = chai.expect;
+
+// Load our module
+const utils = require('../../app/lib/utils');
+
+// Define the available functions
+const utilFunctions = [
+  'color-string',
+  'nest-options',
+  'uid'
+];
+
+// Convert string to camelCase
+function camelCase(string) {
+  return string.replace(/-([a-z])/ig, function(all, letter) {
+    return letter.toUpperCase();
+  });
+}
+
 // Describe the module
 describe('Utils module', () => {
 
+  it('should export an object', () => {
+    expect(utils).to.be.an('object');
+  });
+
+  it('should contain the ' + utilFunctions.length + ' expected functions', () => {
+
+    expect(Object.keys(utils)).to.have.length(utilFunctions.length);
+
+    utilFunctions.forEach((func) => {
+
+      let property = camelCase(func);
+
+      expect(utils[property]).to.be.a('function');
+    });  
+  });
+
   // Load sub-module specs
-  require('./utils/color-string.spec');
-  require('./utils/nest-options.spec');
-  require('./utils/uid.spec');
+  utilFunctions.forEach((func) => {
+    require('./utils/' + func + '.spec');
+  });
 
 });
