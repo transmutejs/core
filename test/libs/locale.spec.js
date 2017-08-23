d04ff733c25da78e1644cd5605f3f210a74c21fc
Jamie Holdroyd <jamie@moltin.com>
2017-08-24 00:53:50 +0100

    Move lang to locale and update tests

---
+++ test/libs/locale.spec.js
@@ -0,0 +1,40 @@
+'use strict';
+
+// Load chai
+const chai = require('chai');
+const expect = chai.expect;
+
+// Load our module
+const i18n = require('../../app/lib/locale');
+
+// Describe the module
+describe('Locale module', () => {
+
+  it('should export an object', () => {
+    expect(i18n).to.be.a('object');
+  });
+
+  it('should add "lang" function to the global space', () => {
+    expect(lang).to.be.a('function');
+  });
+
+  it('should add "plural" function to the global space', () => {
+    expect(plural).to.be.a('function');
+  });
+
+  it('should default to the locale specified in env', () => {
+    expect(i18n.getLocale()).to.be.a('string')
+                            .that.equals( process.env.LANG || 'en' );
+  });
+
+  it('should allow changing of the localization', () => {
+
+    i18n.setLocale('de');
+    
+    expect(i18n.getLocale()).to.be.a('string')
+                            .that.equals('de');
+
+    i18n.setLocale( process.env.LANG || 'en' );
+  });
+
+});
