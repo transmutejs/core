d59fde03954f4474638f7876aa808f30062ffbec
Jamie Holdroyd <jamie@moltin.com>
2017-08-24 00:49:58 +0100

    Add requirements to logger spec

--- test/libs/log.spec.js
+++ test/libs/log.spec.js
@@ -1,10 +1,17 @@
 'use strict';
 
+// Load chai
+const chai = require('chai');
+const expect = chai.expect;
+
+// Load our module
+const logger = require('../../app/lib/log');
+
 // Describe the module
 describe('Log module', () => {
 
-  it('should do something...', () => {
-
+  it('should export an object', () => {
+    expect(logger).to.be.a('object');
   });
 
 });
