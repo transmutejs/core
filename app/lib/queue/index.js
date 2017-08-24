3a902047c494f2390bc64f287c74ed86fb494e12
Jamie Holdroyd <jamie@moltin.com>
2017-08-24 23:02:49 +0100

    Add promise queue to queue handler

--- app/lib/queue/index.js
+++ app/lib/queue/index.js
@@ -1,6 +1,21 @@
 'use strict';
 
+// Load requirements
+const pq = require('promise-queue');
+
+// Build the module structure
 module.exports = {
+
+  // Create paused queue instance
+  items: new pq(0, Infinity),
+
+  // Total number of items added to the queue
+  total: 0,
+
+  // Current task number being run
+  current: 0,
+
+  // Load our sub-modules
   format: require('./format'),
   listing: require('./listing'),
   add: require('./add')
