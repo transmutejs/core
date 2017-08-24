ccd3ce37baf03ed3e9a3ae745d7552a96e9a2af1
Jamie Holdroyd <jamie@moltin.com>
2017-08-24 17:54:15 +0100

    Add comments and update array handling for queue formats

--- app/lib/queue/format.js
+++ app/lib/queue/format.js
@@ -2,69 +2,90 @@
 
 module.exports = {
 
+  // The default formats to support
   formats: ['mkv', 'mp4', 'avi', 'flv', 'mov', 'wmv'],
 
+  // Regex pattern to handle extension matching of the above formats
   pattern: /\.(mkv|mp4|avi|flv|mov|wmv)$/,
 
+  // Add a new format to the available list
   add: function(format) {
 
+    // Check for an array and recurse
     if ( Array.isArray(format) ) {
+      return format.all((item) => { return this.add(item); });
 
-      return format.all((item) => {
-        return this.add(item);
-      });
-
+    // Not a string, can't do anything with it
     } else if ( typeof format !== 'string' ) {
       return false;
     }
 
+    // Already in the list, skip but don't fail
     if ( this.formats.include(format) ) {
       return true;
     }
 
+    // Update formats and regex pattern
     this.formats.push(format);
-
     this.pattern = this.regex();
 
     return true;
   },
 
+  // Remove a format 
   remove: function(format) {
 
-    if ( typeof format !== 'string' ) {
+    // Check for an array and recurse
+    if ( Array.isArray(format) ) {
+      return format.all((item) => { return this.remove(item); });
+
+    // Not a string, can't do anything with it
+    } else if ( typeof format !== 'string' ) {
       return false;
     }
 
+    // Not in the list, skip but don't fail
     if ( ! this.formats.include(format) ) {
       return true;
     }
 
+    // Remove and update regex pattern
     this.formats.splice(this.formats.indexOf(format), 1);
-
     this.pattern = this.regex();
 
     return true;
   },
 
+  // Clear the available formats
   clear: function() {
 
+    // Empty the array
     this.formats = [];
 
+    // Add a generic regex pattern
     this.pattern = /\.([a-z0-9]{2,5})$/gi;
 
     return true;
   },
 
+  // Builds a regex pattern from the format arary
   regex: function() {
     return new RegExp('\.(' + this.formats.join('|') + ')$');
   },
 
+  // Does a given filename/path match the available formats
   match: function(file) {
 
-    if ( typeof file !== 'string' ) {
+    // Check for an array and recurse
+    if ( Array.isArray(file) ) {
+      return file.all((item) => { return this.match(item); });
+
+    // Not a string, can't do anything with it
+    } else if ( typeof file !== 'string' ) {
       return false;
     }
 
+    // Send back the result
     return file.match(this.pattern);
   }
 
