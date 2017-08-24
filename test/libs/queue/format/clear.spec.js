764eb4ea1660a5cfd8397406f6f417449a850613
Jamie Holdroyd <jamie@moltin.com>
2017-08-24 20:06:11 +0100

    Add format clear spec

--- test/libs/queue/format/clear.spec.js
+++ test/libs/queue/format/clear.spec.js
@@ -10,8 +10,31 @@ const format = require('../../../../app/lib/queue/format');
 // Describe the module
 describe('Function "clear"', () => {
 
+  beforeEach(() => { format.reset(); });
+
+  afterEach(() => { format.reset(); });
+
   it('should export a function', () => {
     expect(format.clear).to.be.a('function');
+  });
+
+  it('should remove all items', () => {
+
+    let clearResult = format.clear();
+
+    expect(clearResult).to.be.true;
+    expect(format.formats).to.be.empty;
+  });
+
+  it('should update the regex pattern on change to allow any format', () => {
+
+    let clearResult = format.clear();
+
+    expect(clearResult).to.be.true;
+
+    let matchResult = 'test file.foo'.match(format.pattern);
+
+    expect(matchResult).to.not.be.null;
   });
 
 });
