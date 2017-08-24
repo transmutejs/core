e746677fb92afe48cba5fe0ceb2f082c59d5316e
Jamie Holdroyd <jamie@moltin.com>
2017-08-25 00:02:18 +0100

    Update queue add spec rules

--- test/libs/queue/add.spec.js
+++ test/libs/queue/add.spec.js
@@ -21,4 +21,30 @@ describe('Function "add"', () => {
     expect(addResult).to.be.a('promise');
   });
 
+  it('should add x items to the queue', () => {
+
+  });
+
+  it('should increase the total queue length', () => {
+
+  });
+
+  it('should handle an empty or missing directory', () => {
+
+  });
+
+  it('should cache items as they are added to queue', () => {
+
+  });
+
+  it('should not duplicate queue items', () => {
+
+  });
+
+  it('should throw an error when passed an invalid file', () => {
+
+  });
+
+  
+
 });
