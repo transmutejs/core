88126908072fb6548cdacd7adf80da8dd9b10f20
Jamie Holdroyd <jamie@moltin.com>
2017-08-24 19:27:26 +0100

    Fix default formats reference issue

--- app/lib/queue/format.js
+++ app/lib/queue/format.js
@@ -1,14 +1,18 @@
 'use strict';
 
+// Load requirements
+const logger = require('../log');
+
+// Define default formats
 const defaultFormats = ['mkv', 'mp4', 'avi', 'flv', 'mov', 'wmv'];
 
 module.exports = {
 
   // The default formats to support
-  formats: defaultFormats,
+  formats: defaultFormats.slice(0),
 
   // Regex pattern to handle extension matching of the above formats
-  pattern: new RegExp('\.(' + defaultFormats.join('|') + ')$'),
+  pattern: new RegExp('\.(' + defaultFormats.join('|') + ')$', 'i'),
 
   // Add a new format to the available list
   add: function(format) {
@@ -26,6 +30,9 @@ module.exports = {
     if ( this.formats.includes(format) ) {
       return true;
     }
+
+    // DEBUG
+    logger.verbose('Adding - ' + format);
 
     // Update formats and regex pattern
     this.formats.push(format);
@@ -47,9 +54,12 @@ module.exports = {
     }
 
     // Not in the list, skip but don't fail
-    if ( ! this.formats.include(format) ) {
+    if ( ! this.formats.includes(format) ) {
       return true;
     }
+
+    // DEBUG
+    logger.verbose('Removing - ' + format);
 
     // Remove and update regex pattern
     this.formats.splice(this.formats.indexOf(format), 1);
@@ -65,7 +75,10 @@ module.exports = {
     this.formats = [];
 
     // Add a generic regex pattern
-    this.pattern = /\.([a-z0-9]{2,5})$/gi;
+    this.pattern = /\.([a-z0-9]{2,5})$/i;
+
+    // DEBUG
+    logger.verbose('Clearing formats');
 
     return true;
   },
@@ -74,15 +87,18 @@ module.exports = {
   reset: function() {
 
     // Reset and generate regex pattern
-    this.formats = defaultFormats;
+    this.formats = defaultFormats.slice(0);
     this.pattern = this.regex();
+
+    // DEBUG
+    logger.verbose('Reset formats');
 
     return true;
   },
 
   // Builds a regex pattern from the format arary
   regex: function() {
-    return new RegExp('\.(' + this.formats.join('|') + ')$');
+    return new RegExp('\.(' + this.formats.join('|') + ')$', 'i');
   },
 
   // Does a given filename/path match the available formats
@@ -98,7 +114,7 @@ module.exports = {
     }
 
     // Send back the result
-    return file.match(this.pattern);
+    return file.trim().match(this.pattern);
   }
 
 };
