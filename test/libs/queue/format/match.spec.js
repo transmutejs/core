4ca96d05ccb2ddd94ff083f9599a4d9f0c502a8c
Jamie Holdroyd <jamie@moltin.com>
2017-08-24 20:06:17 +0100

    Add format match space

--- test/libs/queue/format/match.spec.js
+++ test/libs/queue/format/match.spec.js
@@ -14,4 +14,39 @@ describe('Function "match"', () => {
     expect(format.match).to.be.a('function');
   });
 
+  it('should match a valid file format', () => {
+
+    let matchResult = format.match('valid file.mkv');
+
+    expect(matchResult).to.be.true;
+  });
+
+  it('should reject an invalid file format', () => {
+
+    let matchResult = format.match('invalid file.ext');
+
+    expect(matchResult).to.be.false;
+  });
+
+  it('should handle an array of files to match', () => {
+
+    let matchResult = format.match(['valid file 1.mkv', 'valid file 2.mp4']);
+
+    expect(matchResult).to.be.true;
+  });
+
+  it('should reject an array on a single failure', () => {
+
+    let matchResult = format.match(['valid file.mkv', 'invalid file.ext']);
+
+    expect(matchResult).to.be.false;
+  });
+
+  it('should handle an invalid or no argument given', () => {
+
+    let matchResult = format.match();
+
+    expect(matchResult).to.be.false;
+  });
+
 });
