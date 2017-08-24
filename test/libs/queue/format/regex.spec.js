641ae21668a5295db0ce186910951aadcfb610c9
Jamie Holdroyd <jamie@moltin.com>
2017-08-24 20:06:27 +0100

    Add regex format spec

--- test/libs/queue/format/regex.spec.js
+++ test/libs/queue/format/regex.spec.js
@@ -10,8 +10,34 @@ const format = require('../../../../app/lib/queue/format');
 // Describe the module
 describe('Function "regex"', () => {
 
+  beforeEach(() => { format.reset(); });
+
+  afterEach(() => { format.reset(); });
+
   it('should export a function', () => {
     expect(format.regex).to.be.a('function');
+  });
+
+  it('should update the pattern property', () => {
+
+    let addResult = format.add('foo');
+
+    expect(addResult).to.be.true;
+
+    let matchResult = 'test file.foo'.match(format.pattern);
+
+    expect(matchResult).to.not.be.null;
+  });
+
+  it('should handle regex unsafe characters', () => {
+
+    let addResult = format.add('fo*o');
+
+    expect(addResult).to.be.true;
+
+    let matchResult = 'test file.fo*o'.match(format.pattern);
+
+    expect(matchResult).to.not.be.null;
   });
 
 });
