6fce758ff7c43a057d88157eb31e2fc3e949516c
Jamie Holdroyd <jamie@moltin.com>
2017-08-26 00:11:54 +0100

    Move error formatting into validation object

--- app/libs/task/validate.js
+++ app/libs/task/validate.js
@@ -7,24 +7,24 @@ const ajv = require('ajv')({allErrors: true, removeAdditional: 'all'});
 ajv.addSchema(require('./schema/tasks.schema'), 'tasks');
 ajv.addSchema(require('./schema/task.schema'), 'task');
 
-// Error formatting
-function error(errors) {
-  return {
-    status: false,
-    errors: errors.map((err) => {
-      return {path: err.dataPath, message: err.message};
-    })
-  };
-}
-
 // Build the module structure
 module.exports = {
+
+  // Error formatting
+  error: (errors) => {
+    return {
+      status: false,
+      errors: errors.map((err) => {
+        return {path: err.dataPath || '', message: err.message || ''};
+      })
+    };
+  },
 
   // Validate a single task object
   object: (obj) => {
 
     if ( ! ajv.validate('task', obj) ) {
-      return error(ajv.errors);
+      return this.error(ajv.errors);
     }
 
     return {status: true, errors: []};
@@ -34,7 +34,7 @@ module.exports = {
   file: (obj) => {
 
     if ( ! ajv.validate('tasks', obj) ) {
-      return error(ajv.errors);
+      return this.error(ajv.errors);
     }
 
     return {status: true, errors: []};
