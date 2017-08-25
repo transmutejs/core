4ef2a43b03ef119c6681ce2fd904382698e14fc6
Jamie Holdroyd <jamie@moltin.com>
2017-08-26 00:12:05 +0100

    Update the task format reset spec tests

--- test/libs/task/format/reset.spec.js
+++ test/libs/task/format/reset.spec.js
@@ -14,4 +14,32 @@ describe('Function "reset"', () => {
     expect(format.reset).to.be.a('function');
   });
 
+  it('should reset the available formats', () => {
+
+    let defaultFormats = format.formats.slice(0);
+
+    let addResult = format.add('foo');
+
+    expect(addResult).to.be.true;
+
+    expect(format.formats).to.have.length.above(defaultFormats.length);
+
+    format.reset();
+
+    expect(format.formats).to.eql(defaultFormats);
+  });
+
+  it('should update the regex pattern on reset', () => {
+
+    let addResult = format.add('foo');
+
+    expect(addResult).to.be.true;
+
+    format.reset();
+
+    let matchResult = 'test file.foo'.match(format.pattern);
+
+    expect(matchResult).to.be.null;
+  });
+
 });
