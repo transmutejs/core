08646cebaabed882a96b48c9c36b179fe81801d5
Jamie Holdroyd <jamie@moltin.com>
2017-08-25 23:01:00 +0100

    Add temp reject/resolve to details functions

--- app/libs/details/tv.js
+++ app/libs/details/tv.js
@@ -1,9 +1,13 @@
 'use strict';
 
 // Send back the function
-module.exports = function(base) {
+module.exports = function(filename) {
   return new Promise((resolve, reject) => {
 
+    if ( filename === undefined ) {
+      return reject('filename not defined');
+    }
 
+    return resolve();
   });
 };
