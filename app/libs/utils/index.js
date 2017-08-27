26ba69dfc6a3a532057f54f6795479ef4f904f9b
Jamie Holdroyd <jamie@moltin.com>
2017-08-27 01:19:39 +0100

    Add metadata to utils object

--- app/libs/utils/index.js
+++ app/libs/utils/index.js
@@ -4,5 +4,6 @@ module.exports = {
   colorString: require('./color-string'),
   nestOptions: require('./nest-options'),
   uid: require('./uid'),
-  request: require('./request')
+  request: require('./request'),
+  metadata: require('./metadata')
 };
