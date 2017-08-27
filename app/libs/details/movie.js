16810bf501efe66e8c6b886701d819c5e5cb4c41
Jamie Holdroyd <jamie@moltin.com>
2017-08-27 17:17:32 +0100

    Update movie details to use source methods

--- app/libs/details/movie.js
+++ app/libs/details/movie.js
@@ -1,13 +1,28 @@
 'use strict';
 
-// Send back the function
+// Load requirements
+const parser = require('parse-torrent-name');
+
+// Create promise to resolve with dataset
 module.exports = function(filename) {
   return new Promise((resolve, reject) => {
 
+    // No file provided
     if ( filename === undefined ) {
-      return reject('filename not defined');
+      return reject('No filename provided');
     }
 
-    return resolve();
+    // Start our movie details source
+    let source = require('./movie/' + ( process.env.TMDB_KEY ? 'tmdb' : 'tvmaze' ));
+
+    // Decode details from filename
+    let details = parser(filename);
+
+    // Get the movie details
+    source.findMovie(details.title).then((movie) => {
+      return resolve(movie);
+    }).catch((err) => {
+      return reject(err);
+    });
   });
 };
