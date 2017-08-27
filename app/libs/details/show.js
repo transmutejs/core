76802272d91f5d7585d3ca688a597e3ba52b6cdb
Jamie Holdroyd <jamie@moltin.com>
2017-08-27 20:33:55 +0100

    Update show source handling

--- app/libs/details/show.js
+++ app/libs/details/show.js
@@ -7,13 +7,20 @@ const parser = require('parse-torrent-name');
 module.exports = function(filename) {
   return new Promise((resolve, reject) => {
 
+    // Variables
+    let source;
+
     // No file provided
     if ( filename === undefined ) {
       return reject('No filename provided');
     }
 
-    // Start our show details source
-    let source = require('./show/' + ( process.env.TMDB_KEY ? 'tmdb' : 'tvmaze' ));
+    // Check for tmdb key
+    if ( process.env.TMDB_KEY ) {
+      source = require('./show/tmdb')(process.env.TMDB_KEY);
+    } else {
+      source = require('./show/tvmaze')();
+    }
 
     // Decode episode details from filename
     let details = parser(filename);
