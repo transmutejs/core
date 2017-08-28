60810c9a3b286f8554111e97c769d462afa90ac4
Jamie Holdroyd <jamie@moltin.com>
2017-08-28 21:47:18 +0100

    Overhaul engine to use promises and start to add config options

--- app/libs/engine/index.js
+++ app/libs/engine/index.js
@@ -7,19 +7,53 @@ const ffmpeg = require('fluent-ffmpeg'),
 // Configures the conversion task
 module.exports = {
 
-  config: function(file, options, metadata, error) {
+  config: function(file, options, metadata, details) {
+    return new Promise((resolve, reject) => {
 
-    // Create an instance of ffmpeg
-    let command = ffmpeg(file)
-                    .renice(10)                          // Lower the cpu priorty for encode tasks
-                    .outputOptions('-map_metadata', '0') // Keep metadata, title, cover image, etc.
-                    .outputOptions('-map_chapters', '0') // Keep chapter data intact
-                    .outputOptions('-hide_banner');      // Stops ffmpeg diagnostic output
+      // Combine defaults with options
+      options = this.utils.assignDefaults(options);
 
-    // Temp
-    command.output(path.resolve('./sample.mp4'));
+      // Create an instance of ffmpeg
+      let command = ffmpeg(file)
+                      .renice(10)                          // Lower the cpu priorty for encode tasks
+                      .outputOptions('-map_metadata', '0') // Keep metadata, title, cover image, etc.
+                      .outputOptions('-map_chapters', '0') // Keep chapter data intact
+                      .outputOptions('-hide_banner');      // Stops ffmpeg diagnostic output
 
-    return command;
-  }
+      // Loop over the available methods and configure the command
+      this.utils.getMethods(this).reduce((promise, method) => {
+        
+        // Chain the promises
+        return promise.then((result) => {
+
+          // If we got an object back, try to assign to command
+          if ( typeof result === 'object' ) {
+            Object.keys(result).forEach((key) => {
+              if ( ! command.hasOwnProperty(key) ) { command[key] = result[key]; }
+            });
+          }
+
+          // Start the next setup command
+          return this[method](command, file, options, metadata, details);
+        });
+
+      // Resolve to conversion task with command
+      }, Promise.resolve()).then((result) => {
+        return resolve(command);
+
+      // Reject on error
+      }).catch((err) => {
+        return reject(err);
+      });
+    });
+  },
+
+  utils: require('./utils'),
+
+  outputPath: require('./methods/output-path'),
+
+  videoBitrate: require('./methods/video-bitrate'),
+
+  videoSize: require('./methods/video-size')
 
 };
