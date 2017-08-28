5d318b6da7e8cf5c02df5b543ebe21fcd2036699
Jamie Holdroyd <jamie@moltin.com>
2017-08-28 19:05:34 +0100

    Update conversion error handling and rejection

--- app/libs/conversion/events/error.js
+++ app/libs/conversion/events/error.js
@@ -4,9 +4,14 @@
 const logger = require('../../log');
 
 // On conversion error
-module.exports = function(err, stdout, stderr) {
+module.exports = function(err, stdout, stderr, job, metadata, resolve, reject) {
+
+  // Close progress bar
+  this.progressBar.terminate();
 
   // DEBUG
   logger.error(err);
 
+  // Send back to main thread with error
+  return reject(err);
 };
