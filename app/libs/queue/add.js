ada32a96de8499513ae6482a8074076f2bbd5d7e
Jamie Holdroyd <jamie@moltin.com>
2017-08-28 02:58:58 +0100

    Update queue addition

--- app/libs/queue/add.js
+++ app/libs/queue/add.js
@@ -1,5 +1,8 @@
 'use strict';
 
+// Load requirements
+const assign = require('deep-assign');
+
 // Variables
 let queueCache = [];
 
@@ -7,12 +10,65 @@ let queueCache = [];
 module.exports = function(task) {
   return new Promise((resolve, reject) => {
 
+    // Variables
+    let added = [],
+        promises = [],
+        options = {};
+
     // Exit without a task
-    if ( ! task || typeof task !== 'function' ) {
+    if ( ! task || typeof task !== 'object' ) {
       return reject('Missing or invalid task argument');
     }
 
-    // Resolve for now
-    return resolve();
+    // Loop through and assign the available jobs to the queue
+    task.jobs.forEach((job) => {
+
+      // Skip if already in queue
+      if ( queueCache.includes(job.file) ) {
+        return false;
+      }
+
+      // Add to cache to stop additional runs
+      queueCache.push(job.file);
+      this.total = queueCache.length;
+      
+      // Track files being added with this task
+      added.push(job.basename);
+
+      // Merge options for this task
+      job.options = assign(options, job.options);
+
+      // Add to the queue
+      promises.push(this.items.add(() => {
+
+        // Update current task position
+        this.current++;
+
+        // Update overall counter
+        task.overall = {total: this.total, current: this.current};
+
+        // DEBUG
+        console.log('[{cyan:Queue}] Starting task %s of %s', this.current, this.total);
+
+        // Send to conversion for processing
+        return require('../conversion').run(job);
+      }));
+
+    });
+
+    // DEBUG
+    console.log('Added:\n  ' + added.join('  \n').trim());
+
+    // Unpause the queue
+    this.items.maxPendingPromises = 1;
+    this.items._dequeue();
+
+    // Wait for this task to complete to resolve
+    Promise.all(promises).then((result) => {
+      return resolve(result);
+    }).catch((err) => {
+      console.log(err);
+      return reject(err);
+    });
   });
 };
