1ed6360ed48ed9431e2ec1efce3eb891671ba201
Jamie Holdroyd <jamie@moltin.com>
2017-08-28 04:11:43 +0100

    Add progress bar creation on conversion start

--- app/libs/conversion/events/start.js
+++ app/libs/conversion/events/start.js
@@ -1,9 +1,30 @@
 'use strict';
 
+// Load requirements
+const progress = require('progress');
+
+// Load libraries
+const utils = require('../../utils'),
+      logger = require('../../log');
+
 // On conversion start
 module.exports = function(cmd, job, metadata) {
 
-  // DEBUG
-  console.log('Starting');
+  // Verbose
+  logger.debug('Spawned ffmpeg with cmd:\n\n' + cmd);
 
+  // Notify user
+  logger.info('Starting conversion of "{green:%s}".', job.basename);
+
+  // Create a progress bar
+  this.progressBar = new progress(
+    utils.colorString(lang('PROGRESS.CONVERSION')), {
+    complete: '=',
+    incomplete: ' ',
+    head: '>',
+    clear: true,
+    renderThrottle: 0,
+    total: 100,
+    width: 30
+  });
 };
