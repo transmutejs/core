b28700012b58b0d6f5931564243ccc980050d7dc
Jamie Holdroyd <jamie@moltin.com>
2017-08-28 04:13:09 +0100

    Close progress bar on completion

--- app/libs/conversion/events/end.js
+++ app/libs/conversion/events/end.js
@@ -1,9 +1,18 @@
 'use strict';
 
+// Load libraries
+const logger = require('../../log');
+
 // On conversion completion
 module.exports = function(stdout, stderr, job) {
 
+  // Set the progress bar to 100%
+  this.progressBar.update(1);
+
+  // Close progress bar
+  this.progressBar.terminate();
+
   // DEBUG
-  console.log('Complete');
+  logger.info('Complete');
 
 };
--- app/libs/conversion/events/error.js
+++ app/libs/conversion/events/error.js
@@ -1,9 +1,12 @@
 'use strict';
 
+// Load libraries
+const logger = require('../../log');
+
 // On conversion error
 module.exports = function(err, stdout, stderr) {
 
   // DEBUG
-  console.log(err);
+  logger.error(err);
 
 };
