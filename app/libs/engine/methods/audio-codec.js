403226ba32051df6c68b8f779649ea5c86524aee
Jamie Holdroyd <jamie@moltin.com>
2017-08-29 06:07:06 +0100

    Update engine to check for valid audio codec

--- app/libs/engine/methods/audio-codec.js
+++ app/libs/engine/methods/audio-codec.js
@@ -1,11 +1,28 @@
 'use strict';
 
- // Assign the audio codec to use
+// Assign the audio codec to use
 module.exports = function(command, file, options, metadata, details) {
   return new Promise((resolve, reject) => {
 
-    command.audioCodec(options.audio.codec);
+    // Are we copying source?
+    if ( options.audio.codec === 'copy' ) {
 
-    return resolve();
+      // Get the codec data from the audio stream
+      options.audio.codec = command.streams.audio[0].codec_name;
+    }
+
+    // Get a list of the available codecs
+    command.getAvailableCodecs((err, codecs) => {
+
+      // Check we have this one and can encode it
+      if ( ! codecs[options.audio.codec] || ! codecs[options.audio.codec].canEncode ) {
+        return reject('Codec "' + options.audio.codec + '" is not available for encoding.');
+      }
+
+      // Assign audio codec
+      command.audioCodec(options.audio.codec);
+
+      return resolve();
+    });
   });
 };
