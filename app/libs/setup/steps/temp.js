f7497c1e3f5b2ee83989ac5a3a63b2352e17b3c8
Jamie Holdroyd <jamie@moltin.com>
2017-08-30 21:23:44 +0100

    Update setup temp to use new language strings and add validation

--- app/libs/setup/steps/temp.js
+++ app/libs/setup/steps/temp.js
@@ -5,25 +5,33 @@ const path = require('path'),
       fs = require('fs'),
       os = require('os');
 
-// Load modules
-const utils = require(__base + 'libs/utils');
+// Load our modules
+const utils = require(__base + 'libs/utils'),
+      config = require('../config')();
 
 module.exports = {
   type: 'input',
   name: 'temp',
-  message: 'Fantastic! Just one last thing before we move on, where is your temp directory?\n',
+  message: utils.colorString(lang('setup.temp.question')),
   default: os.tmpdir() + ( os.platform() === 'win32' ? '\\' : '/' ),
   when: () => {
+    utils.output(lang('setup.temp.title'), lang('setup.temp.message.why') + '\n\n' + lang('setup.temp.message.options'), 4, 6);
     return true;
   },
   validate: (val, answers) => {
 
+    val = val.trim();
+
+    if( val === '') {
+      return true;
+    }
+
     if ( ! val.match(/[/|\/]$/) ) {
-      return 'Paths must end with a slash';
+      return lang('setup.temp.validation.slash');
     }
 
     if ( ! fs.existsSync(val) || ! fs.lstatSync(val).isDirectory() ) {
-      return 'Path either does not exist or is not a directory';
+      return lang('setup.temp.validation.not_found');
     }
 
     return true;
