3d5e93cd916ad79f4770ef7a190f639099c1eaef
Jamie Holdroyd <jamie@moltin.com>
2017-08-30 10:57:38 +0100

    Clean up entry script

--- app/transmute.js
+++ app/transmute.js
@@ -3,42 +3,39 @@
 // Helper to avoid a lot of directory traversing
 global.__base = __dirname + '/';
 
+// Load requirements
+const logger = require('./libs/log'),
+      task = require('./libs/task'),
+      queue = require('./libs/queue');
+
+// Ensure we're ready to go
 require('./libs/setup').then(() => {
 
-  // Load settings
-  const settings = require('./libs/settings');
-
-  // Run CLI and get arguments
+  // Run CLI, get arguments, and begin processing
   const cli = require('./libs/cli'),
         options = cli.start().input();
 
-  // Load required files
-  const task = require('./libs/task'),
-        queue = require('./libs/queue'),
-        logger = require('./libs/log');
-
   // Load tasks from file
-  task.load(settings.directory + '/tasks.json').then((tasks) => {
+  return task.load('tasks.json');
+
+}).then((tasks) => {
     
-    // Extract jobs from tasks
-    let jobs = [];
-    tasks.forEach((t) => { t.jobs.forEach((j, i) => { jobs.push(j); }); });
+  // Variables
+  let jobs = [];
 
-    // Add jobs into queue and convert
-    return queue.add({jobs: jobs});
+  // Extract jobs from tasks
+  tasks.forEach((t) => { t.jobs.forEach((j, i) => { jobs.push(j); }); });
 
-  // Conversion queue complete
-  }).then((complete) => {
-    logger.info('End of queue.');
-    process.exit(0);
+  // Add jobs into queue and convert
+  return queue.add({jobs: jobs});
 
-  // Catch any errors that bubble up
-  }).catch((err) => {
-    console.log(err);
-    process.exit(1);
-  });
+// Conversion queue complete
+}).then((complete) => {
+  logger.info('End of queue');
+  process.exit(0);
 
+// Catch any errors that bubble up
 }).catch((err) => {
-  console.log(err);
+  logger.error(err);
   process.exit(1);
 });
