c0486585113c60d6f13cb0092b83eadf517abe2b
Jamie Holdroyd <jamie@moltin.com>
2017-08-30 23:23:07 +0100

    Move create settings to a separate file

---
+++ app/libs/setup/steps/create-settings.js
@@ -0,0 +1,42 @@
+'use strict';
+
+// Load requirements
+const path = require('path'),
+      fs = require('fs'),
+      os = require('os');
+
+// Load our modules
+const utils = require(__base + 'libs/utils'),
+      config = require('../config')();
+
+module.exports = function(answers) {
+
+  // Build the base settings object
+  let settings = {
+    language: answers.language,
+    platform: {},
+    server: {
+      port: 3001,
+      address: '0.0.0.0'
+    },
+    video: {
+      formats: answers.formats
+    }
+  };
+  
+  // Add os specific settings
+  settings.platform[os.platform()] = {
+    root: answers.root,
+    temp: answers.temp,
+    directories: {
+      show: answers.show,
+      movie: answers.movie
+    }
+  };
+
+  // Write the settings object
+  fs.writeFileSync(
+    path.join(config.directories.settings, 'settings.json'),
+    JSON.stringify(settings, null, 4)
+  );
+};
