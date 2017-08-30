783da50ef4b69f7705bf80f64dd731176691ad82
Jamie Holdroyd <jamie@moltin.com>
2017-08-30 21:24:19 +0100

    Update formats to use language strings

--- app/libs/setup/steps/formats.js
+++ app/libs/setup/steps/formats.js
@@ -4,13 +4,14 @@
 const path = require('path'),
       os = require('os');
 
-// Load modules
-const utils = require(__base + 'libs/utils');
+// Load our modules
+const utils = require(__base + 'libs/utils'),
+      config = require('../config')();
 
 module.exports = {
   type: 'checkbox',
   name: 'formats',
-  message: '\x1BcLast thing I promise!\n\nWhich video formats are you interested in converting?\n',
+  message: lang('setup.formats.question'),
   choices: [
     {name: 'mkv', checked: true},
     {name: 'mp4', checked: true},
@@ -20,17 +21,15 @@ module.exports = {
     {name: 'flv', checked: false}
   ],
   when: () => {
+    utils.output(lang('setup.formats.title'), lang('setup.formats.message'), 5, 6);
     return true;
   },
   validate: function (answer) {
     
     if ( answer.length < 1 ) {
-      return 'You must choose at least one video format';
+      return lang('setup.formats.validation.empty');
     }
     
     return true;
   }
 };
-
-// let ui = new inquirer.ui.BottomBar();
-// ui.updateBottomBar('new bottom bar content');
