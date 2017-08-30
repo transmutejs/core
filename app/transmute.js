4a4ef988a1798e7da780e9ab51a576fc89c8fed9
Jamie Holdroyd <jamie@moltin.com>
2017-08-30 01:33:08 +0100

    Refactor entry point to do setup

--- app/transmute.js
+++ app/transmute.js
@@ -1,33 +1,43 @@
 'use strict';
 
-// Load settings
-const settings = require('./libs/settings');
+// Helper to avoid a lot of directory traversing
+global.__base = __dirname + '/';
 
-// Run CLI and get arguments
-const cli = require('./libs/cli'),
-      options = cli.input();
+require('./libs/setup').then(() => {
 
-// Load required files
-const task = require('./libs/task'),
-      queue = require('./libs/queue'),
-      logger = require('./libs/log');
+  // Load settings
+  const settings = require('./libs/settings');
 
-// Load tasks from file
-task.load('./config/tasks.json').then((tasks) => {
-  
-  // Extract jobs from tasks
-  let jobs = [];
-  tasks.forEach((t) => { t.jobs.forEach((j, i) => { jobs.push(j); }); });
+  // Run CLI and get arguments
+  const cli = require('./libs/cli'),
+        options = cli.start().input();
 
-  // Add jobs into queue and convert
-  return queue.add({jobs: jobs});
+  // Load required files
+  const task = require('./libs/task'),
+        queue = require('./libs/queue'),
+        logger = require('./libs/log');
 
-// Conversion queue complete
-}).then((complete) => {
-  logger.info('End of queue.');
-  process.exit(0);
+  // Load tasks from file
+  task.load(settings.directory + '/tasks.json').then((tasks) => {
+    
+    // Extract jobs from tasks
+    let jobs = [];
+    tasks.forEach((t) => { t.jobs.forEach((j, i) => { jobs.push(j); }); });
 
-// Catch any errors that bubble up
+    // Add jobs into queue and convert
+    return queue.add({jobs: jobs});
+
+  // Conversion queue complete
+  }).then((complete) => {
+    logger.info('End of queue.');
+    process.exit(0);
+
+  // Catch any errors that bubble up
+  }).catch((err) => {
+    console.log(err);
+    process.exit(1);
+  });
+
 }).catch((err) => {
   console.log(err);
   process.exit(1);
