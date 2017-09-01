24cfae44feedf902c23e957f9475348525267265
Jamie Holdroyd <jamie@moltin.com>
2017-09-01 02:46:13 +0100

    Update actions class

--- app/libs/action/index.js
+++ app/libs/action/index.js
@@ -9,33 +9,39 @@ module.exports = {
 
   // Hand options off to actions and let them do their thing
   go: function(options) {
-    utils.getMethods(this.sources).forEach((method) => {
-      logger.verbose('Starting up ' + method);
-      return this.sources[method](options);
+    utils.getMethods(this, ['go']).forEach((method) => {
+      
+      // DEBUG
+      logger.verbose('Starting input source {green:%s}', method);
+      
+      // Setup the input source
+      return this[method](options);
     });
   },
 
-  // Incoming data sources
-  sources: {
+  // General task file handler
+  tasks: require('./sources/tasks'),
 
-    // General task file handler
-    tasks: require('./sources/tasks'),
+  // Watch the task file for updates
+  watchTask: require('./sources/watch-task'),
 
-    // Watch the task file for updates
-    watchTask: require('./sources/watch-task'),
+  // Watch a directory for file changes
+  watchDirectory: require('./sources/watch-directory'),
 
-    // Watch a directory for file changes
-    watchDirectory: require('./sources/watch-directory'),
+  // Socket connections
+  socket: require('./sources/socket'),
 
-    // Socket connections
-    socket: require('./sources/socket')
-  },
+  // API endpoints
+  api: require('./sources/api'),
 
   // Actions to perform
   actions: {
 
     // Creates a task object and returns the job list
-    createTask: require('./actions/create-task')
+    taskCreate: require('./actions/task-create'),
+
+    // Return the current task in process
+    taskCurrent: require('./actions/task-current')
   }
 
 };
