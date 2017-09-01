32c6a6b991b6615e8a10507f0dc1552a75d569a8
Jamie Holdroyd <jamie@moltin.com>
2017-09-01 21:39:42 +0100

    Add param parsing and gzip to server

--- app/libs/utils/methods/server.js
+++ app/libs/utils/methods/server.js
@@ -31,6 +31,11 @@ module.exports = function() {
       name: pkg.name + ' (' + pkg.version + ')'
     });
 
+    // Setup plugins
+    server.use(restify.plugins.bodyParser());
+    server.use(restify.plugins.queryParser({strictNullHandling: true}));
+    server.use(restify.plugins.gzipResponse());
+
     // Create the socket handler
     socket = require('socket.io').listen(server.server);
 
